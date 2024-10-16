0c8836fa64374e233a1bbf64f0d7b5e15f8182e5
Eilay K <22532056+eilayk@users.noreply.github.com>
2024-10-16 18:19:16 -0400

    fix firefox

--- src/background/background.ts
+++ src/background/background.ts
@@ -4,19 +4,20 @@ import { getProfessorFromRmp } from "./helper";
 // https://developer.chrome.com/docs/extensions/reference/api/declarativeContent
 // use declarative content api to ensure action is only active on on the course selection portal
 // TODO: explore css selectors to activate extension once data is loaded
-const rule = {
-    conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: 'www.uocampus.uottawa.ca', pathPrefix: '/psp/' }
-        })
-    ],
-    actions: [new chrome.declarativeContent.ShowPageAction()]
-};
-chrome.runtime.onInstalled.addListener(details => {
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([rule])
-    });
-});
+// TODO: bring this back when I can figure out how to make it comaptible with firefox
+// const rule = {
+//     conditions: [
+//         new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: { hostEquals: 'www.uocampus.uottawa.ca', pathPrefix: '/psp/' }
+//         })
+//     ],
+//     actions: [new chrome.declarativeContent.ShowPageAction()]
+// };
+// chrome.runtime.onInstalled.addListener(details => {
+//     chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+//         chrome.declarativeContent.onPageChanged.addRules([rule])
+//     });
+// });
 
 // Listen for extension click
 chrome.action.onClicked.addListener(tab => {
